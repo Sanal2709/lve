41a4493c391dab83062b36f774639f210123fd8f
Sanal Azydov <sanal88@yandex.ru>
2023-07-09 20:04:42 +0300

    Vertex Buffers - Vulkan Game Engine Tutorial 06

--- first_app.cpp
+++ first_app.cpp
@@ -8,6 +8,7 @@ namespace lve
 {
     FirstApp::FirstApp()
     {
+        loadModels();
         createPipelineLayout();
         createPipeline();
         createCommandBuffers();
@@ -98,7 +99,8 @@ namespace lve
             vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
             lvePipeline->bind(commandBuffers[i]);
-            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+            lveModel->bind(commandBuffers[i]);
+            lveModel->draw(commandBuffers[i]);
 
             vkCmdEndRenderPass(commandBuffers[i]);
 
@@ -108,6 +110,7 @@ namespace lve
             }
         }
     }
+
     void FirstApp::drawFrame()
     {
         uint32_t imageIndex;
@@ -122,6 +125,16 @@ namespace lve
         {
             throw std::runtime_error("failed to present swap chaon image!");
         }
+    }
+
+    void FirstApp::loadModels(){
+      std::vector<LveModel::Vertex> vertices {
+        {{0.0f, -0.5f}},
+        {{0.5f, 0.5f}},
+        {{-0.5f, 0.5f}}
+      };
+
+      lveModel = std::make_unique<LveModel>(lveDevice, vertices);
     }
 
 } // namespace lve
